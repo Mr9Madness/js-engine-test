5a9b0729b3dfd96549680f1ea538f874567bc9aa
Christiaan Sommer <cmsommer@proton.me>
2024-04-17 00:12:06 +0200

    update loop

--- src/core/rootComponent.js
+++ src/core/rootComponent.js
@@ -1,10 +1,36 @@
 
 import { defineComponent, h } from '@vue/runtime-core'
+import { onMounted, onUpdated } from 'vue'
 
 export const root = defineComponent({
     props: ['root'],
     name: 'root',
     setup(props) {
+        let needsUpdate = false
+        let interval
+
+        function renderRoot() {
+            console.log('render')
+        }
+
+        onMounted(() => {
+            interval = setInterval(() => {
+                console.log('loop')
+                if (needsUpdate) {
+                    console.log('needs update') 
+                    renderRoot()
+                    needsUpdate = false
+                }
+            }, 32)
+
+            renderRoot()
+        })
+
+        function scheduleUpdate() {
+            needsUpdate = true
+        }
+        onUpdated(scheduleUpdate)
+
         return () => h('div', [
             h(props.root)
         ])
